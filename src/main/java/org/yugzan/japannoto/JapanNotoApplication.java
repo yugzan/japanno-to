4b5b39678d91ec7d5b7ea65257fb09399c2adf43
yugzan <yugzan@gmail.com>
2015-08-28 02:52:53 +0800

    Initialize project.

---
+++ src/main/java/org/yugzan/japannoto/JapanNotoApplication.java
@@ -0,0 +1,12 @@
+package org.yugzan.japannoto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JapanNotoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JapanNotoApplication.class, args);
+    }
+}
---
+++ src/test/java/org/yugzan/japannoto/JapanNotoApplicationTests.java
@@ -0,0 +1,19 @@
+package org.yugzan.japannoto;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.yugzan.japannoto.JapanNotoApplication;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = JapanNotoApplication.class)
+@WebAppConfiguration
+public class JapanNotoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
